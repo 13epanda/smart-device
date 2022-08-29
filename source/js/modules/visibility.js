e570b3295c5287ba4a5d9794738430a7f6317764
Vera Tikhonova <veratihonova95@gmail.com>
2022-08-30 01:18:45 +0500

    added function visibility text

--- source/js/modules/visibility.js
+++ source/js/modules/visibility.js
@@ -1,13 +1,20 @@
-const changeVisibility = (bgg) => {
-  console.log(bgg);
-  /*const elements = document.querySelectorAll(elementClass);
-  for (let element of elements) {
-    if (element.style.display === 'none') {
-      element.style.display = 'block';
-    } else {
-      element.style.display = 'none';
-    }
-  }*/
+const changeVisibility = () => {
+  const buttons = document.querySelectorAll('.btn-full');
+  for (let button of buttons) {
+    button.addEventListener('click', function() {
+      const elementClass = button.dataset.visibilityClass;
+      const elements = document.querySelectorAll(`.${elementClass}`);
+      for (let element of elements) {
+        if (element.style.display === 'none') {
+          element.style.display = 'block';
+          button.innerHTML = 'Свернуть';
+        } else {
+          element.style.display = 'none';
+          button.innerHTML = 'Подробнее';
+        }
+      }
+    });
+  }
 };
 
 export {changeVisibility};
