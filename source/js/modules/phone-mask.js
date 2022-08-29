7cb3e5882a3461328954d6d09b744f52d7890f0e
Vera Tikhonova <veratihonova95@gmail.com>
2022-08-30 04:43:59 +0500

    fixed eslint

--- source/js/modules/phone-mask.js
+++ source/js/modules/phone-mask.js
@@ -2,11 +2,11 @@ import IMask from 'imask';
 
 const getPhoneMask = () => {
   const inputElements = document.querySelectorAll('input[type="tel"]');
-  console.log(inputElements);
   for (let inputElement of inputElements) {
     const maskOptions = {
       mask: '+{7}(000)000-00-00',
     };
+    // eslint-disable-next-line new-cap
     IMask(inputElement, maskOptions);
   }
 };
